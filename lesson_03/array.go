01f35dc821e9ecfafed57103840918d2860a051b
Pavel <mirprost@gmail.com>
2022-05-06 23:38:54 +0300

    ok

--- lesson_03/array.go
+++ lesson_03/array.go
@@ -3,17 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	generates_text("Audi", 200, 100, "красный")
-	generates_text("BMW", 180, 90, "желтый")
-	generates_text("Kia", 150, 80, "серый")
-	generates_text("Fiat", 300, 110, "малиновый")
-	generates_text("Bentley", 250, 75, "розовый")
-}
+	people1 := [10]string{"user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10"}
+	people2 := [10]string{"user11", "user12", "user13", "user14", "user15", "user16", "user17", "user18", "user19", "user20"}
+	people3 := [10]string{"user21", "user22", "user23", "user24", "user25", "user26", "user27", "user28", "user29", "user30"}
+	var peopleAll []string
+	for _, value := range people1 {
+		peopleAll = append(peopleAll, value)
+	}
+	for _, value := range people2 {
+		peopleAll = append(peopleAll, value)
+	}
+	for _, value := range people3 {
+		peopleAll = append(peopleAll, value)
+	}
 
-func generates_text(brand string, power int, volume int, color string) {
-	fmt.Printf("Это отличный автомобиль марки %s. "+
-		"Мощность его двигателя составляет %d лошадиных сил. "+
-		"У нее большой двигатель объемом %d л. "+
-		"Прекрасный %s цвет - подчеркивает ее элегантность\n",
-		brand, power, volume, color)
+	for i := 0; i < len(peopleAll); i++ {
+		fmt.Println(peopleAll[i])
+	}
 }
