45c93b9d6fc951ad21ced3954d5572befcbcce57
Pavel <mirprost@gmail.com>
2022-05-22 17:17:54 +0300

    ok

--- stepic/array_slice.go
+++ stepic/array_slice.go
@@ -18,3 +18,41 @@ func main() {
 		fmt.Print(el, " ")
 	}
 }
+
+func task1_12_13(){
+	var n int
+    fmt.Scan(&n)
+    a := make([]int, n, n)
+    for i := 0; i < n; i++ {
+        fmt.Scan(&a[i])
+    }
+    fmt.Println(a[3])
+}
+
+func max()  {
+	array := [5]int{}
+	var a int
+	for i:=0; i < 5; i++{
+		fmt.Scan(&a)
+		array[i] = a
+	}
+    max := array[0]
+    for _, el := range array {
+        if el > max {
+            max = el
+        }
+    }
+    fmt.Println(max)
+}
+
+func task1_12_15() {
+    var n int
+    fmt.Scan(&n)
+    a := make([]int, n)
+    for i := 0; i < n; i++ {
+        fmt.Scan(&a[i]) 
+        if i % 2 == 0 {
+            fmt.Print(a[i], " ")
+        }
+    }
+}
